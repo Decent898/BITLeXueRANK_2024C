1c8c00a0387cc1d75d02158526aeedc6fca55df3
xu-haochen898 <13932646+xu-haochen898@user.noreply.gitee.com>
2024-03-15 20:32:55 +0800

    V1.2

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 smtp_server_address = 'smtp.126.com'
 sender_email_address = 'zxxx1304@126.com'
-smtp_password = 'your_smtp_password'
-receiver_email_address = 'your_receiver_email_address'
+smtp_password = 'HDZRKJRSEVKGKCIA'
+receiver_email_address = '1277630739@qq.com'
 website_login_url = 'https://lexue.bit.edu.cn/course/view.php?id=15114'
-user_account = 'your_account'
-user_password = 'your_password'
+user_account = '1120231322'
+user_password = 'Xhc2020029@'
